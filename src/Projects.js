f292b4e95d5d6daef1313eb996483c129a1c7436
Cankat Örüm <52240938+JohnCut@users.noreply.github.com>
2020-08-27 16:30:53 +0300

    styled-components added
    - Project components put into grid via styled-components

--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,40 @@
 import React from "react";
 import PItem from "./ProjectItem";
 import "./Projects.css";
+import styled from "styled-components";
+
+export const Grid = styled.div``;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
 
 function Projects() {
   return (
     <div className="Projects">
       <h2 className="projects-section-title">My Projects</h2>
-      <PItem title="Bildir" desc="Please kill me" />
-      <PItem title="budged" desc="I am serious" />
+      <Grid>
+        <Row>
+          <Col size={1}>
+            <PItem title="Bildir" desc="Please kill me" />
+          </Col>
+          <Col size={1}>
+            <PItem title="Bildir" desc="Please kill me" />
+          </Col>
+        </Row>
+        <Row>
+          <Col size={1}>
+            <PItem title="Bildir" desc="Please kill me" />
+          </Col>
+          <Col size={1}>
+            <PItem title="Bildir" desc="Please kill me" />
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
