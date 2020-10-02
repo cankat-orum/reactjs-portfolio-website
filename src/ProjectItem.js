09635c2d24c92abd967dd83f12399ba35105dab4
Cankat Örüm <52240938+JohnCut@users.noreply.github.com>
2020-10-02 21:08:32 +0300

    Tech and Link props added.
    - Used tech and project link image props added.
    - Link href added to project link images.

--- src/ProjectItem.js
+++ src/ProjectItem.js
@@ -2,18 +2,73 @@ import React from "react";
 import ItemImg from "./img/index-bg.jpg";
 import "./ProjectItem.css";
 
-function ProjectItem({ title, desc }) {
+function ProjectItem({
+  title,
+  desc,
+  itemSrc,
+  techSrc01,
+  techSrc02,
+  techSrc03,
+  linkSrc01,
+  linkSrc02,
+  linkSrc03,
+  linkHref01,
+  linkHref02,
+  linkHref03,
+}) {
   return (
     <div className="ProjectItem">
-      <img className="thumbnail" src={ItemImg}></img>
+      <img className="thumbnail" src={itemSrc}></img>
       <div className="project-info">
-        <div className="links"></div>
+        <div className="techs-used">
+          <img
+            className="tech-img"
+            src={techSrc01}
+            style={techSrc01 == null ? srcStyle : {}}
+          ></img>
+          <img
+            className="tech-img"
+            src={techSrc02}
+            style={techSrc02 == null ? srcStyle : {}}
+          ></img>
+          <img
+            className="tech-img"
+            src={techSrc03}
+            style={techSrc03 == null ? srcStyle : {}}
+          ></img>
+        </div>
         <h5 className="project-title">{title}</h5>
         <p className="project-desc">{desc}</p>
-        <div className="techs-used"></div>
+        <div className="links">
+          <a href={linkHref01} target="_blank">
+            <img
+              className="link-img"
+              src={linkSrc01}
+              style={linkSrc01 == null ? srcStyle : {}}
+            ></img>
+          </a>
+          <a href={linkHref02} target="_blank">
+            <img
+              className="link-img"
+              src={linkSrc02}
+              style={linkSrc02 == null ? srcStyle : {}}
+            ></img>
+          </a>
+          <a href={linkHref03} target="_blank">
+            <img
+              className="link-img"
+              src={linkSrc03}
+              style={linkSrc03 == null ? srcStyle : {}}
+            ></img>
+          </a>
+        </div>
       </div>
     </div>
   );
 }
+
+let srcStyle = {
+  display: "none",
+};
 
 export default ProjectItem;
--- src/Projects.js
+++ src/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import PItem from "./ProjectItem";
 import "./Projects.css";
 import styled from "styled-components";
+import itemSrc from "./img/index-bg.jpg";
 
 export const Grid = styled.div``;
 
@@ -20,18 +21,26 @@ function Projects() {
       <Grid>
         <Row>
           <Col size={1}>
-            <PItem title="Bildir" desc="Please kill me" />
+            <PItem
+              title="Bildir"
+              desc="Please kill me"
+              itemSrc={itemSrc}
+              techSrc01={itemSrc}
+              techSrc02={itemSrc}
+              linkSrc03={itemSrc}
+              linkHref03="https://github.com/JohnCut/budged_app"
+            />
           </Col>
           <Col size={1}>
-            <PItem title="Bildir" desc="Please kill me" />
+            <PItem title="Bildir" desc="Please kill me" itemSrc={itemSrc} />
           </Col>
         </Row>
         <Row>
           <Col size={1}>
-            <PItem title="Bildir" desc="Please kill me" />
+            <PItem title="Bildir" desc="Please kill me" itemSrc={itemSrc} />
           </Col>
           <Col size={1}>
-            <PItem title="Bildir" desc="Please kill me" />
+            <PItem title="Bildir" desc="Please kill me" itemSrc={itemSrc} />
           </Col>
         </Row>
       </Grid>
