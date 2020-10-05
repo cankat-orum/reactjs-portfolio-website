f9b9d067ac7b74cd11e3896429cc93db625ab505
Cankat Örüm <52240938+JohnCut@users.noreply.github.com>
2020-10-05 14:53:03 +0300

    Contact Me section ui design.

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./ContactForm.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import styled from "styled-components";
+
+export const Grid = styled.div``;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
 
 class ContactForm extends React.Component {
   state = {
@@ -31,57 +42,69 @@ class ContactForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>CONTACT ME!!!</h1>
-        <div>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor="full-name">Full Name</Form.Label>
-              <Form.Control
-                id="full-name"
-                name="name"
-                type="text"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control
-                id="email"
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="message">Full Name</Form.Label>
-              <Form.Control
-                id="message"
-                name="message"
-                as="textarea"
-                rows="3"
-                value={this.state.message}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button
-              className="d-inline-block"
-              variant="primary"
-              type="submit"
-              disabled={this.state.disabled}
-            >
-              Send
-            </Button>
+      <div className="ContactFormPage">
+        <h2 className="contact-form-title">CONTACT ME</h2>
+        <div className="contact-form-div">
+          <Grid>
+            <Form onSubmit={this.handleSubmit}>
+              <div className="name-email-div">
+                <Row>
+                  <Col size={1}>
+                    <Form.Group className="name-email">
+                      <Form.Label htmlFor="full-name">Full Name: </Form.Label>
+                      <Form.Control
+                        id="full-name"
+                        name="name"
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col size={1}>
+                    <Form.Group className="name-email">
+                      <Form.Label htmlFor="email">Email: </Form.Label>
+                      <Form.Control
+                        id="email"
+                        name="email"
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </div>
+              <Form.Group className="message-form">
+                <div className="message-label-txt">
+                  <Form.Label htmlFor="message">Your Message: </Form.Label>
+                  <Form.Control
+                    id="message"
+                    name="message"
+                    as="textarea"
+                    rows="3"
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </Form.Group>
+              <Button
+                className="submit-btn"
+                variant="primary"
+                type="submit"
+                disabled={this.state.disabled}
+              >
+                Send
+              </Button>
 
-            {this.state.emailSent === true && (
-              <p className="d-inline success-msg">Email Send</p>
-            )}
-            {this.state.emailSent === false && (
-              <p className="d-inline err-msg">Email Not Send</p>
-            )}
-          </Form>
+              {this.state.emailSent === true && (
+                <p className="d-inline success-msg">Email Send</p>
+              )}
+              {this.state.emailSent === false && (
+                <p className="d-inline err-msg">Email Not Send</p>
+              )}
+            </Form>
+          </Grid>
         </div>
       </div>
     );
